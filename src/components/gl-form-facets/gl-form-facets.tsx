94f64409eb754a49906145f84c83b96ac79c6e9c
Matt Yoder <myoder@ccrpc.org>
2018-04-02 13:35:37 -0500

    Added facet thumbnails.

--- src/components/gl-form-facets/gl-form-facets.tsx
+++ src/components/gl-form-facets/gl-form-facets.tsx
@@ -12,10 +12,19 @@ export class GLFormFacets {
     document.querySelector('ion-nav').push('gl-form-fields', {facet: facetId});
   }
 
+  getThumbnail(facet) {
+    if (facet.image) return (
+      <ion-thumbnail slot="start">
+        <img src={facet.image} />
+      </ion-thumbnail>
+    );
+  }
+
   render() {
     let items = this.facets.map((facet) => {
       return (
         <ion-item button={true} onClick={() => this.showFields(facet.id)}>
+          {this.getThumbnail(facet)}
           {facet.label}
         </ion-item>
       );
