e129c19fe0359dfa6c1950ad4e30806c4e41aaa9
Matt Yoder <myoder@ccrpc.org>
2018-05-08 12:04:16 -0500

    Initial work on like button and controller.

---
+++ src/components/gl-like-controller/gl-like-controller.tsx
@@ -0,0 +1,30 @@
+import { Component, Method } from '@stencil/core';
+
+
+@Component({
+  tag: 'gl-like-controller'
+})
+export class GLLikeController {
+
+  @Method()
+  getCount(feature: any): number {
+    console.log(feature);
+    return 0;
+  }
+
+  @Method()
+  getLiked(feature: any): boolean {
+    console.log(feature);
+    return false;
+  }
+
+  @Method()
+  like(feature: any) {
+    console.log('like', feature);
+  }
+
+  @Method()
+  unlike(feature: any) {
+    console.log('unlike', feature);
+  }
+}
